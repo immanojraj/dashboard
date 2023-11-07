a719a588c9d4b4500b1ef2c299f9432f3d643920
immanojraj <77238730+immanojraj@users.noreply.github.com>
2023-11-07 16:31:08 +0530

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   
   <!-- Custom styles for this template-->
-  <link href="{{ asset('admin/css/sb-admin-2.min.css') }}" rel="stylesheet">
+  <link href="https://manoj-laravel-dashboard-delta.vercel.app/admin/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 <body class="bg-gradient-primary">
   <div class="container">
@@ -69,11 +69,11 @@
     </div>
   </div>
   <!-- Bootstrap core JavaScript-->
-  <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
-  <script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="https://manoj-laravel-dashboard-delta.vercel.app/admin/vendor/jquery/jquery.min.js"></script>
+  <script src="https://manoj-laravel-dashboard-delta.vercel.app/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <!-- Core plugin JavaScript-->
-  <script src="{{ asset('admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+  <script src="https://manoj-laravel-dashboard-delta.vercel.app/admin/vendor/jquery-easing/jquery.easing.min.js"></script>
   <!-- Custom scripts for all pages-->
-  <script src="{{ asset('admin/js/sb-admin-2.min.js') }}"></script>
+  <script src="https://manoj-laravel-dashboard-delta.vercel.app/admin/js/sb-admin-2.min.js"></script>
 </body>
 </html>
